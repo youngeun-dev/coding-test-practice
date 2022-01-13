22993ff9f24fc08fc5c55470cb30f5579d8a4c11
zer0silver <78026977+zer0silver@users.noreply.github.com>
2022-01-13 17:08:07 +0900

    시간초과 조심

--- java/1920.java
+++ java/1920.java
@@ -1,63 +1,93 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
  
 public class Main {
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+        int N = Integer.parseInt(br.readLine());
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        for (int i = 0; i < N; i++) {
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+        
+        Collections.sort(arr);
+        
+        StringBuilder sb = new StringBuilder();
+        for(int val:arr) {
+        	sb.append(val).append('\n');
+        }
+        System.out.println(sb);
+}
+}
+
+
+
+// import java.io.BufferedReader;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.util.Arrays;
+// import java.util.StringTokenizer;
+ 
+// public class Main {
 	
-	public static int search(int[] A, int key, int start, int end) {
-		while(start <= end) {
-			int mid = (start+end)/2;
+// 	public static int search(int[] A, int key, int start, int end) {
+// 		while(start <= end) {
+// 			int mid = (start+end)/2;
 			
-			if(A[mid]==key) {
-				return 1;
-			}
-			else if(A[mid]<key) {
-				start = mid + 1;
-			}
-			else {
-				end = mid - 1;
-			}
-		}
-		return -1;
-	}
+// 			if(A[mid]==key) {
+// 				return 1;
+// 			}
+// 			else if(A[mid]<key) {
+// 				start = mid + 1;
+// 			}
+// 			else {
+// 				end = mid - 1;
+// 			}
+// 		}
+// 		return -1;
+// 	}
 	
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//     public static void main(String[] args) throws IOException {
+//     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
-    	int n = Integer.parseInt(br.readLine());
-    	int arr[] = new int[n];
+//     	int n = Integer.parseInt(br.readLine());
+//     	int arr[] = new int[n];
     	
-    	StringTokenizer st = null;
-    	st = new StringTokenizer(br.readLine()," ");
+//     	StringTokenizer st = null;
+//     	st = new StringTokenizer(br.readLine()," ");
     	
-    	for(int i =0;i<n;i++) {
-    		arr[i]=Integer.parseInt(st.nextToken());
-    	}
+//     	for(int i =0;i<n;i++) {
+//     		arr[i]=Integer.parseInt(st.nextToken());
+//     	}
     	
-    	int m = Integer.parseInt(br.readLine());
-    	int[] key = new int[m];
+//     	int m = Integer.parseInt(br.readLine());
+//     	int[] key = new int[m];
     	
-    	st = new StringTokenizer(br.readLine()," ");
+//     	st = new StringTokenizer(br.readLine()," ");
     
-    	for(int i =0;i<;i++) {
-    		key[i]=Integer.parseInt(st.nextToken());
-    	}
+//     	for(int i =0;i<;i++) {
+//     		key[i]=Integer.parseInt(st.nextToken());
+//     	}
     	
-    	Arrays.sort(arr);
+//     	Arrays.sort(arr);
     	
-    	for(int i=0; i<m;i++) {
-    		if(search(arr,key[i],0,arr.length-1)==1) {
-    			System.out.println(1);
-    		}else {
-    			System.out.println(0);
-    		}
-    	}
+//     	for(int i=0; i<m;i++) {
+//     		if(search(arr,key[i],0,arr.length-1)==1) {
+//     			System.out.println(1);
+//     		}else {
+//     			System.out.println(0);
+//     		}
+//     	}
     	
        
-    }
-}
+//     }
+// }
 
 
 
