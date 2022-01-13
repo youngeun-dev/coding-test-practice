f9b9cba8eb4456cc954723ea3ac288b2ed6621eb
zer0silver <78026977+zer0silver@users.noreply.github.com>
2022-01-13 14:33:37 +0900

    BufferedReader 사용

---
+++ java/2750_2.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Program {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int val:arr) {
+			System.out.println(val);
+		}
+		
+		
+	}
+	
+}
