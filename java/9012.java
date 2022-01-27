5563ab824041b1b08098e4e70e78a824c69ba009
zer0silver <78026977+zer0silver@users.noreply.github.com>
2022-01-27 22:33:31 +0900

    9012 괄호 풀이추가

--- java/9012.java
+++ java/9012.java
@@ -48,3 +48,51 @@ public class Main {
 		
 	}
 }
+
+
+--------------------------------
+	
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < n; i++) {
+			boolean able = true;
+
+			String s = br.readLine();
+			Stack<Character> stack = new Stack<>();
+
+			for (int j = 0; j < s.length(); j++) {
+				char c = s.charAt(j);
+				if (c == '(') {
+					stack.push(c);
+				} else if (c == ')') {
+					if (!stack.empty()) {
+						stack.pop();
+					}else {
+						able = false;
+						break;
+					}
+				}
+			}
+			
+			if(!stack.empty()) able = false; 
+				
+			if (able) {
+				sb.append("YES").append('\n');
+			} else {
+				sb.append("NO").append('\n');
+			}
+		}
+		System.out.println(sb);
+	}
+}
