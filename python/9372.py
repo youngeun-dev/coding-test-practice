65314cc236de10e6ba1a5f3610d72304c580b17e
허영은 <78026977+zer0silver@users.noreply.github.com>
2023-02-20 22:20:14 +0900

    9372 상근이의 여행

---
+++ python/9372.py
@@ -0,0 +1,7 @@
+T = int(input()) # T: 테스트 케이스의 수 
+
+for _ in range(T): 
+    N, M = map(int, input().split()) # N: 국가의 수, M: 비행기의 종류
+    for _ in range(M): # 입력
+        u, v = map(int, input().split())
+    print(N-1)
