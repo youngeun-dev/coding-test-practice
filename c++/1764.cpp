04556a717d005322d47b146ddb134837d4f7b3f0
허영은 <78026977+zer0silver@users.noreply.github.com>
2023-03-07 12:23:50 +0900

    1764 듣보잡

---
+++ c++/1764.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main(){
+    int n,m;
+    map<string, int> list;
+    vector<string> answer;
+    
+    cin >> n >> m;
+
+    
+    for(int i = 0; i < n+m; i++){
+        string name;
+        cin >> name;
+        list[name]++;
+        if(list[name] > 1){
+            answer.push_back(name);
+        }
+    }
+
+    sort(answer.begin(), answer.end());
+    cout << answer.size() << endl;
+    for(string a : answer){
+        cout << a << endl;
+    }
+
+}
