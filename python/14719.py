6f8309b8fcf01402a3185328b7da1f2cd2e43f18
허영은 <78026977+zer0silver@users.noreply.github.com>
2023-03-27 12:02:07 +0900

    14719 빗물

--- python/14719.py
+++ python/14719.py
@@ -1,14 +1,22 @@
-h, w = map(int, input().split())
-world = list(map(int, input().split()))
+import sys
+input = sys.stdin.readline
 
-ans = 0
-for i in range(1, w - 1):
-    left_max = max(world[:i])
-    right_max = max(world[i+1:])
+# 입력
+H, W = map(int, input().split())
+data = list(map(int, input().split()))
 
-    compare = min(left_max, right_max)
+# 빗물의 총량
+answer = 0
 
-    if world[i] < compare:
-        ans += compare - world[i]
+# 맨 앞과 맨 뒤는 빗물이 고일 수 없으므로 1 ~ W-1까지 탐색
+for i in range(1, W - 1):
+    left = max(data[:i]) # 왼쪽에서 가장 높은 블록
+    right = max(data[i+1:]) # 오른쪽에서 가장 높은 블록
+    min_w = min(left, right)
 
-print(ans)
+    # 빗물이 고일 조건 : 양쪽의 블록이 나보다 높으면 됨
+    # 좌우의 존재하는 블록의 최댓값의 최솟값이 현재 블록보다 크다면 빗물이 고인다. 
+    if min_w > data[i]:
+        answer += min_w - data[i]
+
+print(answer)
