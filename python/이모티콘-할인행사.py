923380165fcac1be3ce04a23be667c956d53a95b
허영은 <78026977+zer0silver@users.noreply.github.com>
2023-04-06 17:45:23 +0900

    프로그래머스 이모티콘 할인행사

--- python/이모티콘-할인행사.py
+++ python/이모티콘-할인행사.py
@@ -1,42 +1,45 @@
+from itertools import product
+
 def solution(users, emoticons):
-    answer = [0, 0]
-    data = [10 ,20, 30, 40]
-    discount = []
+    # 사용자 수, 이모티콘 수 
+    n, m = len(users), len(emoticons)
     
-    # 이모티콘 할인율 구하기
-    def dfs(temp, depth):
-        if depth == len(temp):
-            discount.append(temp[:])
-            return
-        for d in data:
-            temp[depth] += d
-            dfs(temp, depth + 1)
-            temp[depth] -= d
+    # 이모티콘 플러스 가입자
+    total_joiner = 0
+    # 이모티콘 매출액
+    total_cost = 0
     
-    dfs([0] * len(emoticons), 0)
+    # 중복 순열로 할인율 조합 찾기 
+    discounts = product([10,20,30,40], repeat = m)
     
-    # 완전탐색
-    for d in range(len(discount)):
-        join, price = 0, [0] * len(users)
-        for e in range(len(emoticons)):
-            for u in range(len(users)):
-                # 할인율을 만족하면 구매
-                if users[u][0] <= discount[d][e]:
-                    price[u] += emoticons[e] * (100 - discount[d][e]) / 100
+    for discount in discounts:
+        # 각 조합별 이모티콘 판매액, 플러스 가입자 수 
+        sum_cost = 0
+        joiner = 0
         
-        # 구매 금액에 따라 가입자 갱신
-        for u in range(len(users)):
-            if price[u] >= users[u][1]:
-                join += 1
-                price[u] = 0
-        
-        # 최대 가입자, 구매 금액 갱신
-        if join >= answer[0]:
-            if join == answer[0]:
-                answer[1] = max(answer[1], sum(price))
+        for user in users:
+            # 사용자의 구매 기준
+            rate, max_cost = user
+            # 사용자의 이모티콘 결제 금액
+            buy_cost = 0
+            
+            for i, emoticon in enumerate(emoticons):
+                if discount[i] >= rate:
+                    # 할인된 가격
+                    buy_cost += emoticon * (100 - discount[i]) * 0.01
+            
+            if buy_cost >= max_cost:
+                joiner += 1
             else:
-                answer[1] = sum(price)
-            answer[0] = join
-    
-    
-    return answer
+                sum_cost += buy_cost
+        
+        # 이모티콘 플러스 가입자가 더 많은 경우 
+        if total_joiner < joiner:
+            total_joiner = joiner
+            total_cost = sum_cost
+        # 가입자 수는 같은데, 판매액이 더 많은 경우 
+        elif total_joiner == joiner:
+            if total_cost < sum_cost:
+                total_cost = sum_cost 
+
+    return [total_joiner, total_cost]
