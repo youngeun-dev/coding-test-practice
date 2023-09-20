1cea37d7de7cdf44bbcde89c4873903510cd53c4
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2023-09-20 19:35:49 +0900

    5073 삼각형과 세 변

--- python/5073.py
+++ python/5073.py
@@ -8,11 +8,11 @@ while True:
     a, b, c = map(int, input().split())
     if a == b == c == 0:
         break
-    elif a == b == c:
+    if a == b == c:
         print("Equilateral")
+    elif max((a, b, c)) >= sum((a, b, c)) - max((a, b, c)):
+        print("Invalid")
     elif a == b or a == c or b == c:
         print("Isosceles")
-    elif max(a, b, c) >= (a + b + c) - max(a, b, c):
-        print("Invalid")
     else:
         print("Scalene")
