602a9c3131d5b41e2a186343da0675412fa16c9f
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2023-09-28 20:48:00 +0900

    2606 바이러스

---
+++ python/2606.py
@@ -0,0 +1,31 @@
+# https://www.acmicpc.net/problem/2606
+# 바이러스
+import sys
+input = sys.stdin.readline
+
+N = int(input()) # 컴퓨터의 수
+M = int(input()) # 연결되어 있는 컴퓨터 쌍의 수
+
+# 연결 되어 있는 컴퓨터 네트워크 맵
+graph = [[] for _ in range(N + 1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 방문 여부 확인
+visited = [False] * (N + 1)
+
+
+def dfs(now):
+    global cnt
+    visited[now] = True
+    for nxt in graph[now]: # 연결된 컴퓨터 찾기
+        if not visited[nxt]:
+            dfs(nxt)
+            cnt += 1
+
+cnt = 0
+dfs(1) # 1번 컴퓨터와 연결된 컴퓨터 개수 찾으러
+
+print(cnt)
