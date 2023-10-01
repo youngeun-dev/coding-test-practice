0fae225a20463f2d41c4697bf0ca871be08acb20
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2023-10-01 15:59:59 +0900

    2503 숫자 야구

--- python/2503.py
+++ python/2503.py
@@ -4,7 +4,7 @@ import sys, itertools
 input = sys.stdin.readline
 
 n = int(input())
-group = list(itertools.permutations(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], 3))
+group = list(itertools.permutations(['1', '2', '3', '4', '5', '6', '7', '8', '9'], 3))
 
 for _ in range(n):
     num, strike, ball = map(int, input().split())
