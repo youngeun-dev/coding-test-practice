68e58c577c824c18706bf3c1595f4f131139c1d2
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2023-10-07 17:12:31 +0900

    17086 아기 상어 2

--- python/17086.py
+++ python/17086.py
@@ -6,7 +6,6 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0] * m for _ in range(n)]
 
 dx = [-1, 0, 1, 0, 1, -1, 1, -1]
 dy = [0, 1, 0, -1, 1, -1, -1, 1]
@@ -17,7 +16,6 @@ for x in range(n):
     for y in range(m):
         if graph[x][y] == 1:
             shack.append((x, y))
-            visited[x][y] = 1
 
 # BFS
 def BFS():
@@ -26,16 +24,14 @@ def BFS():
         for i in range(8):
             nx, ny = x + dx[i], y + dy[i]
             if 0 <= nx < n and 0 <= ny < m:
-                if not visited[nx][ny] and graph[nx][ny] == 0:
-                    visited[nx][ny] = visited[x][y] + 1
+                if graph[nx][ny] == 0:
+                    print(nx, ny)
+                    graph[nx][ny] = graph[x][y] + 1
+
+                    print(graph)
                     shack.append((nx, ny))
 
 
-# 안전 거리의 최댓값 구하기
 BFS()
-answer = 0
-for x in range(n):
-    for y in range(m):
-        answer = max(answer, visited[x][y] - 1)
-
-print(answer)
+# 안전 거리의 최댓값 구하기
+print(max(map(max, graph)) - 1)
