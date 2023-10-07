1649dca27b427630b3aedce49fdae39e92ad2aa1
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2023-10-07 12:55:22 +0900

    15650 N과 M (2)

---
+++ python/15650.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/15650
+# N과 M (2)
+import sys
+input = sys.stdin.readline
+
+
+def dfs(start):
+    if len(result) == m:
+        print(' '.join(map(str, result)))
+        return
+
+    for i in range(start, n + 1):
+        if i not in result:
+            result.append(i)
+            dfs(start + 1)
+            result.pop()
+
+
+n, m = map(int, input().split())
+result = []
+dfs(1)
