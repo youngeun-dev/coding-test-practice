6e2cfaeda47e926c50ae2d847ede48de52cae8ef
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2023-10-29 14:06:36 +0900

    11055 가장 큰 증가하는 부분 수열

---
+++ python/11055.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/11055
+# 가장 큰 증가하는 부분 수열
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+num_list = list(map(int, input().split()))
+dp = [1] * n
+dp[0] = 1
+
+for i in range(1, n):
+    for j in range(i):
+        if num_list[i] > num_list[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
