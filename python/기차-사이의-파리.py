22c44215a152a656daaa1184bc2c8bc525bf3a94
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2023-11-14 14:02:19 +0900

    SWEA 기차 사이의 파리

---
+++ python/기차-사이의-파리.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    D, A, B, F = map(int, input().split())
+    print(f'#{test_case} {(D / (A + B)) * F}')
