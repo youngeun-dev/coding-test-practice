50a9ebd0b647ef695a7079a0b18442577486541e
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2023-11-30 11:18:02 +0900

    COS Pro 4차 1급 2

---
+++ python/cos-4차-1급-2.py
@@ -0,0 +1,14 @@
+def solution(s):
+    s = s.lower()
+    answer = ""
+    previous = s[0]
+    counter = 1
+    for alphabet in s[1:]:
+        if alphabet == previous:
+            counter += 1
+        else:
+            answer += previous + str(counter)
+            counter = 1
+            previous = alphabet
+    answer += previous + str(counter)
+    return answer
