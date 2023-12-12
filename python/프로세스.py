cc7275f5a399b984e4701e25ccaa5d775c4b9b5d
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2023-12-12 14:48:45 +0900

    프로그래머스 프로세스

--- python/프로세스.py
+++ python/프로세스.py
@@ -1,6 +1,6 @@
 def solution(priorities, location):
     answer = [0] * size
-    priorities = [(i, priority) for i, priority in enumerate(priorities)]
+    priorities = [(i, p) for i, p in enumerate(priorities)]
     first = max(priorities, key=lambda x: x[1])[1]
 
     cnt = 1
@@ -16,3 +16,24 @@ def solution(priorities, location):
     answer[priorities[0][0]] = cnt
     
     return answer[location]
+
+
+
+# --------------------------------------------
+# any() 사용
+
+def solution(priorities, location):
+    answer = 0
+    queue = [(i, p) for i, p in enumerate(priorities)]
+
+    while True:
+        idx, priority = queue.pop(0)
+        if any(priority < q[1] for q in queue):
+            queue.append((idx, priority))
+        else:
+            answer += 1
+            if idx == location:
+                break
+    
+    return answer
+    
