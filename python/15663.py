a314d4f0bfeed693c2d03aa49883f01364ec92d5
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2024-01-16 13:25:27 +0900

    15663 N과 M(9)

---
+++ python/15663.py
@@ -0,0 +1,31 @@
+import sys
+
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+
+# 조건을 만족하는 수열
+array = []
+
+# 방문 여부 체크
+visited = [False] * n
+
+# 수열 전체 보관
+result = set()
+
+def backtracking():
+    if len(array) == m:
+        result.add(tuple(array))
+        return 
+    
+    for i in range(n):
+      if not visited[i]:
+        array.append(nums[i])
+        visited[i] = True
+        backtracking()
+        array.pop()
+        visited[i] = False
+
+backtracking()
+
+for r in sorted(result):
+   print(*r)
