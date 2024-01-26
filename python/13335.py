e8cf352a16d1da20008f84e8426d580098a42acd
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2024-01-26 16:25:01 +0900

    13335 트럭

---
+++ python/13335.py
@@ -0,0 +1,20 @@
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+import sys
+
+n, w, l = map(int, input().split())
+trucks = list(map(int, input().split()))
+ 
+bridge = [0] * w
+time = 0
+while bridge:
+	time += 1
+	bridge.pop(0) 
+	if trucks: 
+		if sum(bridge) + trucks[0] <= l:
+			bridge.append(trucks.pop(0))
+		else:
+			bridge.append(0) 
+print(time)
