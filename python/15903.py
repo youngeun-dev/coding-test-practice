d00eba52c68312489c78005f72c3508e96d04ba4
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2024-03-12 23:44:41 +0900

    15903 카드 합체 놀이

--- python/15903.py
+++ python/15903.py
@@ -10,3 +10,20 @@ for _ in range(m):
     cards.sort()
 
 print(sum(cards))
+
+# import sys
+# import heapq as hq
+# input = sys.stdin.readline
+
+# n, m = map(int, input().split())
+# cards = list(map(int, input().split()))
+# hq.heapify(cards)
+
+# for _ in range(m):
+#     card1 = hq.heappop(cards)
+#     card2 = hq.heappop(cards)
+
+#     hq.heappush(cards, card1 + card2)
+#     hq.heappush(cards, card1 + card2)
+
+# print(sum(cards))
