5f8e50ef50b3f65fffd25f0895df756cc8ca0e7e
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2024-06-02 22:24:08 +0900

    프로그래머스 디스크 컨트롤러

--- java/디스크-컨트롤러.java
+++ java/디스크-컨트롤러.java
@@ -1,24 +1,33 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int[][] jobs) {
-        int answer = 0;
-        int lastWork = 0;
+        int totalWorkTime = 0; // 총 작업 시간
+        int idx = 0; // 인덱스
+        int completeJobs = 0; // 수행 완료된 jobs 개수
+        int endTime = 0; // 종료 시간
         
-        // 요청시점, 소요시간
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> (o1[1] - o2[1]));
-
-        for(int i=0; i<jobs.length; i++){
-            pq.offer(jobs[i]);
+        // 시작 시간 오름차순 정렬
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        // 소요 시간 오름차순 정렬
+        PriorityQueue<int []> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        
+        while(completeJobs < jobs.length){
+            // endTime 전에 들어온 요청 pq에 추가
+            while(idx < jobs.length && jobs[idx][0] <= endTime){
+                pq.offer(jobs[idx++]);
+            }
+            
+            if(!pq.isEmpty()){
+                completeJobs++;
+                int[] job = pq.poll();
+                endTime += job[1];
+                totalWorkTime += endTime - job[0];
+            }else{ // pq가 비어있다면 다음 시작 시간으로 endTime 업데이트
+                endTime = jobs[idx][0];
+            }
+            
         }
-        
-        while(!pq.isEmpty()){
-            int[] times = pq.poll();
-            answer += Math.abs(lastWork - times[0]) + times[1];
-            lastWork += times[1];
-        }        
-        
-        return answer / jobs.length;
+        return totalWorkTime / jobs.length;
     }
 }
