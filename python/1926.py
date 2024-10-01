a65007bf2f56f200b5878d782d2d98a2d4e81ee7
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2024-10-01 13:53:04 +0900

    Update 1926.py

--- python/1926.py
+++ python/1926.py
@@ -36,3 +36,45 @@ if result:
 else:
     print(len(result))
     print(0)
+
+
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+
+from collections import deque
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+DRAW = 1
+
+
+def bfs(x, y):
+    q = deque([(x, y)])
+    board[x][y] = -1
+    cnt = 1
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m: continue
+            if board[nx][ny] != DRAW: continue
+            
+            board[nx][ny] = -1
+            q.append((nx, ny))
+            cnt += 1
+    return cnt
+
+
+n, m = map(int, input().split())
+board = list(list(map(int, input().split())) for _ in range(n))
+
+cnt, picture = 0, 0
+for x in range(n):
+    for y in range(m):
+        if board[x][y] == DRAW:
+            cnt += 1
+            size = bfs(x, y)
+            if size > picture: picture = size
+
+print(cnt)
+print(picture)
