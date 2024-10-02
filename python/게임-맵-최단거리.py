4769767fd22d157dd5379f5dea6a95f1e0fe0fa9
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2024-10-02 13:47:32 +0900

    Update 게임-맵-최단거리.py

--- python/게임-맵-최단거리.py
+++ python/게임-맵-최단거리.py
@@ -28,3 +28,31 @@ def solution(maps):
                 q.append([nx, ny])
     
     return -1 if maps[N-1][M-1] == 1 else maps[N-1][M-1]
+
+
+# ---------------------------------------------------------------
+from collections import deque
+
+def bfs(maps):
+    goalX = len(maps)
+    goalY = len(maps[0])
+    q = deque([(0, 0, 1)])
+    maps[0][0] = -1
+    
+    while q:
+        x, y, cnt = q.popleft()
+        if (x, y) == (goalX - 1, goalY - 1):
+            return cnt
+            
+        for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+            nx, ny = x + dx, y + dy
+            if nx < 0 or nx >= goalX or ny < 0 or ny >= goalY: continue
+                
+            if maps[nx][ny] == 1:
+                maps[nx][ny] = -1
+                q.append((nx, ny, cnt + 1))
+                
+    return -1
+    
+def solution(maps):
+    return bfs(maps)
