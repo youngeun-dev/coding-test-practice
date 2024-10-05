9db1484f094763d43985dd6302e53a20441166ce
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2024-10-05 19:35:51 +0900

    1759 암호 만들기

--- python/1759.py
+++ python/1759.py
@@ -27,3 +27,40 @@ aeiou = ['a', 'e', 'i', 'o', 'u']
 code = []
 
 make_code(0)
+
+
+# --------
+import sys
+input = sys.stdin.readline
+
+def check(code):
+    aeiou_cnt = 0
+    not_aeiou_cnt = 0
+    for x in code:
+        if x in aeiou:
+            aeiou_cnt += 1
+        else:
+            not_aeiou_cnt += 1
+
+    if aeiou_cnt >= 1 and not_aeiou_cnt >= 2:
+        print(''.join(code))
+
+
+def back_tracking(index):
+    if len(code) == L:
+        check(code)
+        return
+
+    for nxt in range(index, C):
+        code.append(alphas[nxt])
+        back_tracking(nxt + 1)
+        code.pop()
+
+
+L, C = map(int, input().split())
+alphas = list(input().split())
+alphas.sort()
+
+aeiou = ['a', 'e', 'i', 'o', 'u']
+code = []
+back_tracking(0)
