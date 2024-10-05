2e8b14ee9be40b1c8e830af637efb6b38e2fa2bc
Youngeun Heo <78026977+youngeun-dev@users.noreply.github.com>
2024-10-06 00:03:20 +0900

    1941 소문난 칠공주

--- python/1941.py
+++ python/1941.py
@@ -58,3 +58,60 @@ princess = [] # 학생들의 좌표를 담을 배열
 
 DFS(0, 0)
 print(answer)
+
+
+
+# ------
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+board = [list(input().rstrip()) for _ in range(5)]
+
+def bfs(member):
+    # 기본: 0, 멤버 위치: 1, 방문: 2
+    visited = [[0] * 5 for _ in range(5)]
+
+    for x, y in member:
+        visited[x][y] = 1
+
+    q = deque([member[0]])
+    visited[member[0][0]][member[0][1]] = 2
+    cnt = 1
+    while q:
+        x, y = q.popleft()
+        for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+            nx, ny = x + dx, y + dy
+            if nx < 0 or nx >= 5 or ny < 0 or ny >= 5: continue
+            if visited[nx][ny] == 1:
+                visited[nx][ny] = 2
+                cnt += 1
+                q.append((nx, ny))
+
+    return cnt
+
+
+
+def dfs(start, y_cnt):
+    global answer
+    if y_cnt >= 4:
+        return
+
+    if len(member) == 7:
+        if bfs(member) == 7:
+            answer += 1
+        return
+
+    for i in range(start, 25):
+        x = i // 5
+        y = i % 5
+        member.append((x, y))
+        dfs(i + 1, y_cnt + (board[x][y] == 'Y'))
+        member.pop()
+
+
+answer = 0
+member = []
+dfs(0, 0)
+print(answer)
+
